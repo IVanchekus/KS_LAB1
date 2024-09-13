149fa0e3ba5e4a41e06279602be7e056be88c3fc
IVanchekus <ivan.gravdan@mail.ru>
2024-09-13 18:51:35 +0700

    readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Установка проекта
+
+```
+git clone https://github.com/IVanchekus/KS_LAB1
+
+python -m venv appvenv
+
+# Для Linux
+source ./appvenv/bin/activate
+
+pip install -r requirements.txt
+
+python main.py
+```
--- main.py
+++ main.py
@@ -1,22 +1,23 @@
+from pathlib import Path
 import telebot
 import os
 import uuid
 
-bot = telebot.TeleBot("7382252794:AAES4Js1asYhOzpAEAwdG7mmmAyWDFVHnnc")
-src = "/home/ivan/PROJECTS/DOMASHKA/KOMPZRENIE/lab1/saved_photos/"
+telegram_api_bot = "7382252794:AAES4Js1asYhOzpAEAwdG7mmmAyWDFVHnnc"
+bot = telebot.TeleBot(telegram_api_bot)
 
 @bot.message_handler(content_types=["text"])
 def get_text_message(message):
-    bot.send_message(message.from_user.id, "Тестим")
+    bot.send_message(message.from_user.id, "Прикрепи фотографию для того, чтобы я нашел совпадения")
 
 @bot.message_handler(content_types=["photo"])
 def get_photo_messages(message):
     file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
     downloaded_file = bot.download_file(file_info.file_path)
 
-    full_src = src + uuid.uuid1().hex + os.path.basename(file_info.file_path)
+    full_src = "./saved_photos/" + uuid.uuid1().hex + os.path.basename(file_info.file_path)
 
-    with open(full_src, 'wb') as new_file:
+    with Path.open(full_src, 'wb') as new_file:
         new_file.write(downloaded_file)
 
     bot.reply_to(message, "Сохранили") 
@@ -26,9 +27,9 @@ def get_file_messages(message):
     file_info = bot.get_file(message.document.file_id)
     downloaded_file = bot.download_file(file_info.file_path)
 
-    full_src = src + uuid.uuid1().hex + message.document.file_name
+    full_src = "./saved_photos/" + uuid.uuid1().hex + message.document.file_name
 
-    with open(full_src, 'wb') as new_file:
+    with Path.open(full_src, 'wb') as new_file:
         new_file.write(downloaded_file)
 
     bot.reply_to(message, "Сохранили") 
